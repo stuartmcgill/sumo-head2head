9e1f4df1ee5c862fdcfda3d5fefce489081d3eac
Stuart McGill <stueymcgill@gmail.com>
2023-07-23 19:41:39 +0100

    Add component file

---
+++ public/assets/wrestler.js
@@ -0,0 +1,31 @@
+export default {
+    data() {
+        return {
+            shikonaEn: "Takarafuji",
+            stable: "Isegahama",
+            sumoDbId: 11728
+        }
+    },
+    computed: {
+        imageLink() {
+            return "http://sumodb.sumogames.de/pics/$(sumoDbId).jpg"
+        }
+    },
+    template:
+        `
+<div class="card">
+  <div class="row card-body">
+      <div class="col-sm-6">
+        <h1 class="card-title">{{ shikonaEn }}</h1>
+        <p class="card-text">{{ stable }}</p>
+        <a href="#" class="btn btn-primary">Full record</a>
+      </div>
+      <img
+        src="http://sumodb.sumogames.de/pics/11728.jpg"
+        class="col-sm-6"
+        alt="..."
+      >
+  </div>
+</div>
+`
+}
