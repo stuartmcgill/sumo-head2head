169c74731867f4805f39c69c83e58a4bbf33f389
Stuart McGill <stueymcgill@gmail.com>
2023-07-25 06:17:34 +0100

    Retrieve Makuuchi wrestlers from API

--- src/Controller/Head2HeadController.php
+++ src/Controller/Head2HeadController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use StuartMcGill\SumoApiPhp\Service\RikishiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,19 @@ class Head2HeadController extends AbstractController
     #[Route('head2head')]
     public function number(): Response
     {
-        $number = random_int(0, 100);
+        $filename = __DIR__ . '/../../data/wrestlers.json';
+
+        if (file_exists($filename)) {
+            $wrestlers = file_get_contents($filename);
+        } else {
+            $rikishiService = RikishiService::factory();
+            $wrestlers = $rikishiService->fetchDivision('Makuuchi');
+            $wrestlers = json_encode($wrestlers);
+            file_put_contents(filename: $filename, data: $wrestlers);
+        }
 
         return $this->render('head2head.html.twig', [
-            'number' => $number,
+            'wrestlers' => $wrestlers,
         ]);
     }
 }
